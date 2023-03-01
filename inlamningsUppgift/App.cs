21635616dc6099d9579b24e6bf36fa84019298a5
Susanna Alvkvist <susannaalvkvist@hotmail.com>
2023-03-01 14:58:16 +0100

    Funkar för G

--- inlamningsUppgift/App.cs
+++ inlamningsUppgift/App.cs
@@ -1,19 +1,36 @@
-﻿public class App
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+public class App
 {
-       
-       
-        
 
-
-        public void Run()
+    static int Meny()
+    {
+        while (true)
         {
+
+            Console.WriteLine("******VÄLKOMMEN!******");
+            Console.WriteLine("VÄLJ ETT ALTERNATIV");
+            Console.WriteLine("");
+            Console.WriteLine("1. Spela/spela igen");
+            Console.WriteLine("2. Low Score");
+            Console.WriteLine("3. Avsluta");
+
+
+        }
+
+    }
+
+
+
+    public void Run()
+    {
 
 
         var random = new Random();
         int secretNumber = random.Next(1, 101);
         bool playAgain = true;
-        int counter = 0;
-        int lowScore = 0;
+
 
 
 
@@ -21,37 +38,37 @@
 
 
         Console.WriteLine("Gissa talet mellan 1 och 100.");
-       
+
         while (playAgain)
         {
-          
 
-            counter = 1;
+
+            int counter = 1;
             secretNumber = random.Next(1, 101);
             int tal = 0;
             tal = 0;
-           
+
 
 
             while (tal != secretNumber)
             {
-                tal = Convert.ToInt32(Console.ReadLine());
+                tal = checkNumber();
                 Console.WriteLine($"Runda: {counter}");
-                
+
 
                 if (tal > secretNumber)
                 {
-                    
+
                     Console.WriteLine($"Talet är mindre än {tal}.");
-                    
+
 
                 }
 
                 else if (tal < secretNumber)
                 {
-                   
+
                     Console.WriteLine($"Talet är större än {tal}.");
-                    
+
 
                 }
                 counter++;
@@ -61,22 +78,11 @@
             Console.WriteLine($"{secretNumber} är rätt!");
             Console.WriteLine($"Du gissade rätt på {counter} gånger.");
 
-            Console.WriteLine($"Vill du spela igen? Ja/Nej");
-            string svar = Console.ReadLine();
-           
-            if (svar == "Ja")
-            {
-                
-                playAgain = true;
-                Console.WriteLine("Gissa talet mellan 1 och 100.");
-            }
-            else
-            {
-                playAgain = false;
-            }
-            Console.WriteLine("Tack för den här gången!");
+            playAgain = checkString();
 
             
+
+
 
 
 
@@ -86,10 +92,56 @@
 
     }
 
+    public int checkNumber()
+    {
+        int tal = 0;
+
+        while (true)
+        {
+            
+            if (int.TryParse(Console.ReadLine(), out tal) == false)
+            {
+
+                Console.WriteLine("Felaktig inmatning, ange ett tal");
+                continue;
+            }
+            if (tal < 1 || tal > 100)
+            {
+                Console.WriteLine("Felaktig inmatning, ange ett tal mellan 1 - 100");
+                continue;
+            }
+
+            return tal;
+        }
+    }
+
+
+    public bool checkString()
+    {
+       
+
+        while (true)
+        {
+            Console.WriteLine($"Vill du spela igen? Ja/Nej");
+            string svar = Console.ReadLine().ToLower();
 
 
 
-} 
+            if (svar == "ja")
+            {
+                Console.WriteLine("Gissa talet mellan 1 och 100.");
+                return true;
+                
+            }
+            else if(svar == "nej")
+            {
+                Console.WriteLine("Tack för den här gången!");
+                return false;
+            }
+
+        }
+    }
+}
 
    
 
